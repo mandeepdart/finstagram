cee89b2421993c700362bde30f0fd1aab20c112c
Mandeep Bhaikai Sanghi <mandeepdart@gmail.com>
2017-09-06 02:59:59 +0000

    New commit

--- app/actions.rb
+++ app/actions.rb
@@ -1,12 +1,49 @@
-get '/' do 
-	@posts = Post.order(created_at: :desc)
-	erb(:index) 
-
+helpers do 
+	def current_user 
+		User.find_by(id: session[:user_id])
+	end 
 end 
-get '/signup' do  # if a user navigates to the path "/signup"
-	@user =  User.new # setup empty @user object 
-	erb(:signup) # render "app/views/signup.erb"
+get '/login' do 
+	erb(:login)
+end 
+post '/login' do 
+	username = params[:username]
+	password = params[:password]
 
+	user = User.find_by(username: username)
+
+	if user && user.password == password 
+		session[:user_id] = user.id 
+		redirect to('/')
+	else 
+		@error_message = "Login failed."
+		erb(:login)
+	end 
+end 
+
+get '/logout' do 
+	session[:user_id] = nil 
+	redirect to ('/')
+end 
+get '/' do 
+	@posts =Post.order(created_at: :desc)
+	erb(:index)
+end 
+
+ 
+post '/signup' do 
+	email = params[:email]
+	avatar_url = params[:avatar_url]
+	username = params[:username]
+	password = params[:password]
+
+	@user = User.new({ email: email, avatar_url: avatar_url, username: username, password: password})
+
+	if @user.save 
+		redirect to ('/login')
+	else 
+		erb(:signup)
+	end 
 end 
 post '/signup' do 
 	 
